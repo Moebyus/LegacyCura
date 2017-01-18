3b3b844cd18276a426cc8e1c67f4076056b81eea
moebyusDev <support@moebyus.com>
2017-01-18 02:22:52 +0100

    ajustes en la ventana de selección de modelo

--- Cura/gui/configWizard.py
+++ Cura/gui/configWizard.py
@@ -1161,7 +1161,8 @@ class ConfigWizard(wx.wizard.Wizard):
 #Moebyus mod
 		self.MoebyusSelectModel		= moebyusGui.MoebyusSelectModelPage(self)
 		self.MoebyusDonePage 		= moebyusGui.MoebyusDonePage(self)
-		self.ultimakerSelectModel	= UltimakerMachineSelectPage(self)
+		self.MoebyusFirmwarePage 	= moebyusGui.moebyusFirmwarePage(self)
+		self.ultimakerSelectModel	= UltimakerMachineSelectPage(self)		
 		wx.wizard.WizardPageSimple.Chain(self.machineSelectPage	, self.MoebyusSelectModel)
 		wx.wizard.WizardPageSimple.Chain(self.MoebyusSelectModel, self.MoebyusDonePage)
 
