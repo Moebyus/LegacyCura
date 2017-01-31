6916531ceaaf13afd56389bdef7da7fc10b6dab9
moebyusDev <support@moebyus.com>
2017-01-31 17:11:44 +0100

    fix a las secuencias de arranque de SIRIUS

--- Cura/util/moebyusFactory.py
+++ Cura/util/moebyusFactory.py
@@ -159,7 +159,6 @@ M117 Terminado!
 ;{profile_string}
 """)
 
-
 def setAlterationsM3() :
 	profile.setAlterationFile('start.gcode', """;Moebyus start GCODE for M3
 ;Sliced at: {day} {date} {time}
@@ -210,6 +209,46 @@ M117 Materializando...
 """)		
 
 def setAlterationsSirius() :
+	profile.setAlterationFile('start.gcode', """;Default Moebyus SIRIUS start GCODE
+;Sliced at: {day} {date} {time}
+;Basic settings: Layer height: {layer_height} Walls: {wall_thickness} Fill: {fill_density}
+;Print time: {print_time}
+;Filament used: {filament_amount}m {filament_weight}g
+;Filament cost: {filament_cost}
+;M190 S{print_bed_temperature} ;Uncomment to add your own bed temperature line
+;M109 S{print_temperature} ;Uncomment to add your own temperature line
+G21                     ;metric values
+G90                     ;absolute positioning
+M82                     ;set extruder to absolute mode
+M107                    ;start with the fan off
+G28
+G29                     ;Run the auto bed leveling
+G1 X0 Y0 Z5.0 F{travel_speed} 
+G92 E0                  ;zero the extruded length
+G1 F300 E25             ;extrude
+G92 E0                  ;zero the extruded length again
+G0 Z0.5 Y20 F1000
+G0 Y50
+G1 F{travel_speed}
+;Put printing message on LCD screen
+M117 Materializando...
+""")
+
+	profile.setAlterationFile('end.gcode', """;Default Moebyus end GCODE
+;Sliced at: {day} {date} {time}
+;End GCode
+M104 S0                    ;extruder heater off
+M140 S0                    ;heated bed heater off (if you have it)
+G91                        ;relative positioning
+G1 E-2.5 F300              ;retract the filament a bit before lifting the nozzle, to release some of the pressure
+G1 Z+2 E-3 F{travel_speed} ;move Z up a bit and retract filament even more
+G28 XY                     ;move X/Y to min endstops, so the head is out of the way
+M84                        ;steppers off
+G90                        ;absolute positioning
+M117 Terminado!
+;{profile_string}
+""")
+
 	profile.setAlterationFile('start2.gcode', """;Moebyus start2 GCODE for SIRIUS
 ;Sliced at: {day} {date} {time}
 ;Basic settings: Layer height: {layer_height} Walls: {wall_thickness} Fill: {fill_density}
@@ -313,7 +352,6 @@ T0
 	
 def setAlterationsSiriusDuplicator() :
 	profile.setAlterationFile('start.gcode', """;Moebyus start GCODE for SIRIUS duplicator
-
 M605 S2
 M117 Duplicator Enabled!
 G28
